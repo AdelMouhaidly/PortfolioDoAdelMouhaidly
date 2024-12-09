e3f2f5cb6c7dc9bd0f2d745f6771c181a231eb71
Adel <rm557705@fiap.com.br>
2024-12-09 03:46:07 -0300

    Correção do código

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,31 @@
+import { useState } from 'react';
 import styles from './Header.module.css';
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>
-        <span className={styles.nomeBranco}>Adel Mouhaidly</span> | Meu Portfólio
-      </div>
-      <nav>
-        <ul className={styles.navList}>
+      <div className={styles.logo}>Adel Mouhaidly | <span className={styles.portfolio}>Meu Portfólio</span></div>
+      
+      <nav className={`${styles.navList} ${menuOpen ? styles.showMenu : ''}`}>
+        <ul>
           <li><a href="#sobre">Sobre</a></li>
           <li><a href="#habilidades">Habilidades</a></li>
           <li><a href="#projetos">Projetos</a></li>
-          <li><a href="#contato">Contato</a></li> 
+          <li><a href="#contato">Contato</a></li>
         </ul>
       </nav>
+
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+      </div>
     </header>
   );
 }
